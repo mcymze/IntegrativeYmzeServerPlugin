4a764ef9800bae32c5d4afc43c052be29aea3b96
ekuinox <depkey@me.com>
2020-02-12 14:34:52 +0900

    #2 playerとitemが満たしてる場合のみに処理するというのがしたい

--- src/main/scala/dev/ekuinox/IntegrativeYmzeServerPlugin/services/phantomcoping/listeners/PlayerItemConsumeEventListener.scala
+++ src/main/scala/dev/ekuinox/IntegrativeYmzeServerPlugin/services/phantomcoping/listeners/PlayerItemConsumeEventListener.scala
@@ -2,18 +2,30 @@ package dev.ekuinox.IntegrativeYmzeServerPlugin.services.phantomcoping.listeners
 
 import dev.ekuinox.IntegrativeYmzeServerPlugin.utils.EventListener
 import dev.ekuinox.IntegrativeYmzeServerPlugin.{Main => Plugin}
+import org.bukkit.Material
 import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.player.PlayerItemConsumeEvent
+import org.bukkit.inventory.ItemStack
 
 class PlayerItemConsumeEventListener(plugin: Plugin) extends EventListener(plugin) {
+  import PlayerItemConsumeEventListener._
+  import dev.ekuinox.IntegrativeYmzeServerPlugin.services.phantomcoping.Permissions._
 
   @EventHandler
   def onPlayerItemConsumeEvent(event: PlayerItemConsumeEvent): Unit = {
-    val player = event.getPlayer
-    val item = event.getItem
-    player.sendMessage(s"${player.getName}が${item.getType.toString}を消費しとる")
+    for {
+      player <- event.getPlayer.withCoping
+      item <- event.getItem.withTarget
+    } {
 
+    }
+  }
+}
 
+object PlayerItemConsumeEventListener {
+  val TARGET_ITEM: Material = Material.ROTTEN_FLESH
+  implicit class Item(item: ItemStack) {
+    def withTarget: Option[ItemStack] = if (item.getType == TARGET_ITEM) Some(item) else None
   }
 }
