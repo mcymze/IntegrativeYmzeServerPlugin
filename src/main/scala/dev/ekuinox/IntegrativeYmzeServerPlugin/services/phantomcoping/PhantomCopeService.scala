d7811a8dc4479677a5d98dbf99adcc852d9012b0
ekuinox <depkey@me.com>
2020-02-12 14:11:25 +0900

    #2 とりあえずregisterしてる

--- src/main/scala/dev/ekuinox/IntegrativeYmzeServerPlugin/services/phantomcoping/PhantomCopeService.scala
+++ src/main/scala/dev/ekuinox/IntegrativeYmzeServerPlugin/services/phantomcoping/PhantomCopeService.scala
@@ -1,5 +1,6 @@
 package dev.ekuinox.IntegrativeYmzeServerPlugin.services.phantomcoping
 
+import dev.ekuinox.IntegrativeYmzeServerPlugin.services.phantomcoping.listeners.PlayerItemConsumeEventListener
 import dev.ekuinox.IntegrativeYmzeServerPlugin.{Main => Plugin}
 
 /**
@@ -7,12 +8,13 @@ import dev.ekuinox.IntegrativeYmzeServerPlugin.{Main => Plugin}
  * @param plugin Plugin
  */
 class PhantomCopeService(plugin: Plugin) {
+  lazy val playerItemConsumeEventListener = new PlayerItemConsumeEventListener(plugin)
 
   /**
    * このクラスで扱うListenerをまとめて登録する
    */
   def registerListeners(): Unit = {
-
+    playerItemConsumeEventListener.register()
   }
 
 }
