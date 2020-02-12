adec0cf694e6abff54b5f13e6c9c96b5bbe8ec3a
ekuinox <depkey@me.com>
2020-02-13 02:24:49 +0900

    #2 ImplicitConversionsをimportして使う

--- src/main/scala/dev/ekuinox/IntegrativeYmzeServerPlugin/services/phantomcoping/listeners/PlayerItemConsumeEventListener.scala
+++ src/main/scala/dev/ekuinox/IntegrativeYmzeServerPlugin/services/phantomcoping/listeners/PlayerItemConsumeEventListener.scala
@@ -1,20 +1,18 @@
 package dev.ekuinox.IntegrativeYmzeServerPlugin.services.phantomcoping.listeners
 
-import dev.ekuinox.IntegrativeYmzeServerPlugin.services.phantomcoping.PhantomCopeService
-import dev.ekuinox.IntegrativeYmzeServerPlugin.utils.{EventListener, Service}
-import dev.ekuinox.IntegrativeYmzeServerPlugin.{Main => Plugin}
+import dev.ekuinox.IntegrativeYmzeServerPlugin.services.phantomcoping.{Permissions, PhantomCopeService, Timer, ImplicitConversions}
+import dev.ekuinox.IntegrativeYmzeServerPlugin.utils.EventListener
 import org.bukkit.Material
-import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.player.PlayerItemConsumeEvent
 import org.bukkit.inventory.ItemStack
 
 class PlayerItemConsumeEventListener(implicit service: PhantomCopeService) extends EventListener {
   import PlayerItemConsumeEventListener._
-  import dev.ekuinox.IntegrativeYmzeServerPlugin.services.phantomcoping.Permissions._
 
   @EventHandler
   def onPlayerItemConsumeEvent(event: PlayerItemConsumeEvent): Unit = {
+    import ImplicitConversions._
     for {
       player <- event.getPlayer.withCoping
       item <- event.getItem.withTarget
