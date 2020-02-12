6e5275dafedd9e557c3308b31706d6ea5d4622d0
ekuinox <depkey@me.com>
2020-02-12 23:41:17 +0900

    #2 ServiceからNamespacedKeyを生成する

--- src/main/scala/dev/ekuinox/IntegrativeYmzeServerPlugin/utils/Service.scala
+++ src/main/scala/dev/ekuinox/IntegrativeYmzeServerPlugin/utils/Service.scala
@@ -1,7 +1,11 @@
 package dev.ekuinox.IntegrativeYmzeServerPlugin.utils
 
 import dev.ekuinox.IntegrativeYmzeServerPlugin.Main
+import org.bukkit.NamespacedKey
 
 class Service(implicit val main: Main) {
   def getPlugin: Main = main
+
+  def makeNamespacedKey(key: String): NamespacedKey = new NamespacedKey(main, s"${this.toString}.key")
+
 }
