ca4e8d03e1ac2b911f309147d3079f4e7c9899a4
ekuinox <depkey@me.com>
2020-02-12 23:52:00 +0900

    #2 containerとnamespacedKeyを変数にした

--- src/main/scala/dev/ekuinox/IntegrativeYmzeServerPlugin/services/phantomcoping/Timer.scala
+++ src/main/scala/dev/ekuinox/IntegrativeYmzeServerPlugin/services/phantomcoping/Timer.scala
@@ -11,12 +11,14 @@ object Timer {
   val PERSISTENT_DATA_CONTAINER_TYPE: PersistentDataType[lang.Byte, lang.Byte] = PersistentDataType.BYTE
 
   implicit class Player(player: BukkitPlayer)(implicit service: PhantomCopeService) {
+    private val container = player.getPersistentDataContainer
+    private val namespacedKey = service.makeNamespacedKey(NAMESPACED_KEY)
 
     /**
      * Playerがファントムの追跡を解除できるか
      * @return Boolean
      */
-    def canCope: Boolean = player.getPersistentDataContainer.has(service.makeNamespacedKey(NAMESPACED_KEY), PERSISTENT_DATA_CONTAINER_TYPE)
+    def canCope: Boolean = container.has(namespacedKey, PERSISTENT_DATA_CONTAINER_TYPE)
 
 
   }
