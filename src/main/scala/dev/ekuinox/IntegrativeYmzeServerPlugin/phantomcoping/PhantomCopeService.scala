d80604b156de1d33554d418afea6403dd1db88b1
ekuinox <depkey@me.com>
2020-02-12 13:42:40 +0900

    #2 ファントムを寄せ付けないための処理をここで管理する

---
+++ src/main/scala/dev/ekuinox/IntegrativeYmzeServerPlugin/phantomcoping/PhantomCopeService.scala
@@ -0,0 +1,18 @@
+package dev.ekuinox.IntegrativeYmzeServerPlugin.phantomcoping
+
+import dev.ekuinox.IntegrativeYmzeServerPlugin.{Main => Plugin}
+
+/**
+ * ファントムの寄ってくるのが鬱陶しいので、寄ってこないようにする
+ * @param plugin Plugin
+ */
+class PhantomCopeService(plugin: Plugin) {
+
+  /**
+   * このクラスで扱うListenerをまとめて登録する
+   */
+  def registerListeners(): Unit = {
+
+  }
+
+}
