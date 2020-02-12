8df44092dc2f368984c096309d67b540f10cc66b
ekuinox <depkey@me.com>
2020-02-12 23:23:57 +0900

    #2 Service を継承して implicit valを渡すように

--- src/main/scala/dev/ekuinox/IntegrativeYmzeServerPlugin/services/phantomcoping/PhantomCopeService.scala
+++ src/main/scala/dev/ekuinox/IntegrativeYmzeServerPlugin/services/phantomcoping/PhantomCopeService.scala
@@ -1,14 +1,16 @@
 package dev.ekuinox.IntegrativeYmzeServerPlugin.services.phantomcoping
 
 import dev.ekuinox.IntegrativeYmzeServerPlugin.services.phantomcoping.listeners.PlayerItemConsumeEventListener
+import dev.ekuinox.IntegrativeYmzeServerPlugin.utils.Service
 import dev.ekuinox.IntegrativeYmzeServerPlugin.{Main => Plugin}
 
 /**
  * ファントムの寄ってくるのが鬱陶しいので、寄ってこないようにする
  * @param plugin Plugin
  */
-class PhantomCopeService(plugin: Plugin) {
-  lazy val playerItemConsumeEventListener = new PlayerItemConsumeEventListener(plugin)
+class PhantomCopeService(implicit plugin: Plugin) extends Service {
+  implicit val self: PhantomCopeService = this
+  lazy val playerItemConsumeEventListener = new PlayerItemConsumeEventListener
 
   /**
    * このクラスで扱うListenerをまとめて登録する
