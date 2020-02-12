038961a7e0bd87809592808f193c4966a29bad11
ekuinox <depkey@me.com>
2020-02-12 23:23:21 +0900

    #2 EventListenerがServiceを受け取るように変更
    ServiceからgetPluginするよう

--- src/main/scala/dev/ekuinox/IntegrativeYmzeServerPlugin/utils/EventListener.scala
+++ src/main/scala/dev/ekuinox/IntegrativeYmzeServerPlugin/utils/EventListener.scala
@@ -1,10 +1,9 @@
 package dev.ekuinox.IntegrativeYmzeServerPlugin.utils
 
-import dev.ekuinox.IntegrativeYmzeServerPlugin.{Main => Plugin}
 import org.bukkit.event.{Listener => BukkitListener}
 
-class EventListener(plugin: Plugin) extends BukkitListener {
+class EventListener(implicit service: Service) extends BukkitListener {
   def register(): Unit = {
-    plugin.getServer.getPluginManager.registerEvents(this, plugin)
+    service.getPlugin.getServer.getPluginManager.registerEvents(this, service.getPlugin)
   }
 }
