918de80d1887f9220ba897535a267f6304ab0f16
ekuinox <depkey@me.com>
2020-02-14 00:21:57 +0900

    #2 ListenerContainer にまかせて...

--- src/main/scala/dev/ekuinox/IntegrativeYmzeServerPlugin/services/phantomcoping/PhantomCopeService.scala
+++ src/main/scala/dev/ekuinox/IntegrativeYmzeServerPlugin/services/phantomcoping/PhantomCopeService.scala
@@ -1,22 +1,20 @@
 package dev.ekuinox.IntegrativeYmzeServerPlugin.services.phantomcoping
 
-import dev.ekuinox.IntegrativeYmzeServerPlugin.services.phantomcoping.listeners.PlayerItemConsumeEventListener
-import dev.ekuinox.IntegrativeYmzeServerPlugin.utils.Service
+import dev.ekuinox.IntegrativeYmzeServerPlugin.services.phantomcoping.listeners.{EntityTargetEventListener, PlayerItemConsumeEventListener}
+import dev.ekuinox.IntegrativeYmzeServerPlugin.utils.{EventListener, ListenerContainer, Service}
 import dev.ekuinox.IntegrativeYmzeServerPlugin.{Main => Plugin}
 
 /**
  * ファントムの寄ってくるのが鬱陶しいので、寄ってこないようにする
  * @param plugin Plugin
  */
-class PhantomCopeService(implicit plugin: Plugin) extends Service {
+class PhantomCopeService(implicit plugin: Plugin) extends Service with ListenerContainer{
   implicit val self: PhantomCopeService = this
-  lazy val playerItemConsumeEventListener = new PlayerItemConsumeEventListener
 
-  /**
-   * このクラスで扱うListenerをまとめて登録する
-   */
-  def registerListeners(): Unit = {
-    playerItemConsumeEventListener.register()
-  }
+  // このクラスで利用するListener
+  override val eventListeners = Seq(
+    new PlayerItemConsumeEventListener,
+    new EntityTargetEventListener
+  )
 
 }
