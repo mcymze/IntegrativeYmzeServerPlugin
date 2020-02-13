1f1298adc785037e5916b1ec13c7369a27857c64
ekuinox <depkey@me.com>
2020-02-14 00:26:05 +0900

    #2 Seqにして他のServiceがきても大丈夫なように...

--- src/main/scala/dev/ekuinox/IntegrativeYmzeServerPlugin/Main.scala
+++ src/main/scala/dev/ekuinox/IntegrativeYmzeServerPlugin/Main.scala
@@ -1,14 +1,17 @@
 package dev.ekuinox.IntegrativeYmzeServerPlugin
 
 import dev.ekuinox.IntegrativeYmzeServerPlugin.services.phantomcoping.PhantomCopeService
+import dev.ekuinox.IntegrativeYmzeServerPlugin.utils.{ListenerContainer, Service}
 import org.bukkit.plugin.java.JavaPlugin
 
 class Main extends JavaPlugin {
   implicit val self: Main = this
-  lazy val phantomCopeService = new PhantomCopeService
+  lazy val services: Seq[Service with ListenerContainer] = Seq(
+    new PhantomCopeService
+  )
 
   override def onEnable(): Unit = {
-    phantomCopeService.registerListeners()
+    services.foreach(_.registerListeners())
   }
 
 }
