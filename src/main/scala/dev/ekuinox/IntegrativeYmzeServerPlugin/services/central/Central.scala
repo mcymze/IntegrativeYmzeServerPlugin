20ebd4a3ecb1b5f56adda7839bfa4188e07ec609
ekuinox <depkey@me.com>
2020-02-14 12:14:37 +0900

    reload を Central#onCommand で待ち受ける
    このヘンはベタ書きって感じなので書き直したい

--- src/main/scala/dev/ekuinox/IntegrativeYmzeServerPlugin/services/central/Central.scala
+++ src/main/scala/dev/ekuinox/IntegrativeYmzeServerPlugin/services/central/Central.scala
@@ -2,6 +2,10 @@ package dev.ekuinox.IntegrativeYmzeServerPlugin.services.central
 
 import dev.ekuinox.IntegrativeYmzeServerPlugin.Main
 import dev.ekuinox.IntegrativeYmzeServerPlugin.utils.{EventListener, Service}
+import org.bukkit.command.CommandSender
+import org.bukkit.entity.Player
+
+import scala.util.Try
 
 /**
  * プラグイン自体の管理だとかそういうのをやる
@@ -10,6 +14,17 @@ class Central(implicit main: Main) extends Service {
   import Central._
   override val name: String = NAME
   override val eventListeners: Seq[EventListener] = Seq.empty
+
+  override def onCommand(sender: CommandSender, args: List[String]): Unit = {
+    args.headOption.foreach {
+      case "reload" => {
+        Try(sender.asInstanceOf[Player]).toOption match {
+          case Some(player) if player.hasPermission("ymze.central.reload") => reloadConfig()
+          case None => reloadConfig()
+        }
+      }
+    }
+  }
 }
 
 object Central {
