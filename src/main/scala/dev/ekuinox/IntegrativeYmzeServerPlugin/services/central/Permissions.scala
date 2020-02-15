db06f6f45291db7bf23f2d5df32a39f4d478a0fd
ekuinox <depkey@me.com>
2020-02-15 14:11:29 +0900

    #9 CentralにReload Permissionを作成して利用

---
+++ src/main/scala/dev/ekuinox/IntegrativeYmzeServerPlugin/services/central/Permissions.scala
@@ -0,0 +1,9 @@
+package dev.ekuinox.IntegrativeYmzeServerPlugin.services.central
+
+import dev.ekuinox.IntegrativeYmzeServerPlugin.utils.Permissions.Permission
+
+object Permissions {
+
+  object Reload extends Permission("reload")
+
+}
