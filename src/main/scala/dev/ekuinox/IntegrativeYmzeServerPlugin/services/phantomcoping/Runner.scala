8efa0505315c147e21f97fe7211a01c02c759cdb
ekuinox <depkey@me.com>
2020-02-16 19:40:51 +0900

    #22 runnersの管理もRunner側でやる

--- src/main/scala/dev/ekuinox/IntegrativeYmzeServerPlugin/services/phantomcoping/Runner.scala
+++ src/main/scala/dev/ekuinox/IntegrativeYmzeServerPlugin/services/phantomcoping/Runner.scala
@@ -2,8 +2,11 @@ package dev.ekuinox.IntegrativeYmzeServerPlugin.services.phantomcoping
 
 import org.bukkit.entity.Player
 import org.bukkit.scheduler.BukkitRunnable
+import collection.mutable.{Map => MutableMap}
 
 class Runner(player: Player, effectiveTicks: Long)(implicit service: PhantomCopeService) extends BukkitRunnable {
+  import Runner._
+
   private var spentTicks = 0L
 
   override def run(): Unit = {
@@ -13,22 +16,23 @@ class Runner(player: Player, effectiveTicks: Long)(implicit service: PhantomCope
     // 有効時間を経過
     if (spentTicks > effectiveTicks) {
       player.deactivateCoping()
-      Timer.timers.remove(player)
+      runners.remove(player)
       cancel()
     }
   }
 
   def getSpentTicks: Long = spentTicks
 
-  def start(): (Player, Runner) = {
+  def start(): Unit = {
     // 毎Tick呼び出す
     runTaskTimer(service.getPlugin, 0L, 1L)
-    player -> this
+    runners += (player -> this)
   }
 
 }
 
 object Runner {
+  val runners: collection.mutable.Map[Player, Runner] = MutableMap[Player, Runner]()
 
   def apply(player: Player, effectiveTicks: Long)(implicit service: PhantomCopeService): Runner = new Runner(player, effectiveTicks)(service)
 
