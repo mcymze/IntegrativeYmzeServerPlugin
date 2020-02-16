01376df827e977dc141e10a739b730dee520433e
ekuinox <depkey@me.com>
2020-02-16 19:38:26 +0900

    #22 開始自体は外でやってもらうように

--- src/main/scala/dev/ekuinox/IntegrativeYmzeServerPlugin/services/phantomcoping/Runner.scala
+++ src/main/scala/dev/ekuinox/IntegrativeYmzeServerPlugin/services/phantomcoping/Runner.scala
@@ -6,9 +6,6 @@ import org.bukkit.scheduler.BukkitRunnable
 class Runner(player: Player, effectiveTicks: Long)(implicit service: PhantomCopeService) extends BukkitRunnable {
   private var spentTicks = 0L
 
-  // 毎Tick呼び出す
-  runTaskTimer(service.getPlugin, 0L, 1L)
-
   override def run(): Unit = {
     import Timer._
     spentTicks += 1
@@ -22,6 +19,13 @@ class Runner(player: Player, effectiveTicks: Long)(implicit service: PhantomCope
   }
 
   def getSpentTicks: Long = spentTicks
+
+  def start(): (Player, Runner) = {
+    // 毎Tick呼び出す
+    runTaskTimer(service.getPlugin, 0L, 1L)
+    player -> this
+  }
+
 }
 
 object Runner {
