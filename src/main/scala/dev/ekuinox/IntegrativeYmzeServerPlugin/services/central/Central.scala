8cafff3e9338ed9660054e40211ec1d41a245977
ekuinox <depkey@me.com>
2020-02-18 17:59:03 +0900

    ダメージ別でやれないか試してる

--- src/main/scala/dev/ekuinox/IntegrativeYmzeServerPlugin/services/central/Central.scala
+++ src/main/scala/dev/ekuinox/IntegrativeYmzeServerPlugin/services/central/Central.scala
@@ -2,8 +2,12 @@ package dev.ekuinox.IntegrativeYmzeServerPlugin.services.central
 
 import dev.ekuinox.IntegrativeYmzeServerPlugin.Main
 import dev.ekuinox.IntegrativeYmzeServerPlugin.utils.{EventListener, Service}
+import org.bukkit.Material
 import org.bukkit.command.CommandSender
+import org.bukkit.inventory.meta.Damageable
 import org.bukkit.entity.Player
+import org.bukkit.inventory.{ItemFlag, ItemStack}
+import org.bukkit.inventory.meta.ItemMeta
 
 import scala.util.Try
 
@@ -19,6 +23,7 @@ class Central(implicit main: Main) extends Service {
   override def onCommand(sender: CommandSender, args: List[String]): Unit = {
     args.headOption.foreach {
       case "reload" => onReloadCommand(sender, args.tail)
+      case _ => onDefaultCommand(sender, args)
     }
   }
 
@@ -34,6 +39,35 @@ class Central(implicit main: Main) extends Service {
       reloadConfig()
       sender.sendServiceMessage("Reloaded configurations")
     })
+  }
+
+  private def onDefaultCommand(sender: CommandSender, args: List[String]): Unit = {
+
+    val mat = Material.valueOf(args.headOption.getOrElse("WOODEN_HOE"))
+    val damage = args.lastOption.getOrElse("1").toIntOption.getOrElse(1)
+
+    Try(sender.asInstanceOf[Player]).toOption.foreach(player => {
+
+      val item = new ItemStack(Material.WOODEN_HOE, 1)
+      val currentDamage = Try(item.getItemMeta.asInstanceOf[Damageable]).toOption.map(damageable => {
+        damageable.setDamage(damage)
+        item.setItemMeta(damageable.asInstanceOf[ItemMeta])
+        damageable.getDamage
+      })
+
+      item.setItemMeta({
+        val meta = item.getItemMeta
+        meta.addItemFlags(ItemFlag.HIDE_ATTRIBUTES, ItemFlag.HIDE_UNBREAKABLE)
+        meta.setUnbreakable(true)
+        meta.setDisplayName("Central Powder")
+        meta
+      })
+
+      player.getInventory.setItemInMainHand(item)
+      player.sendMessage(s"アイテムをあげるわ...あなたに $currentDamage, ${item.getDurability}")
+
+    })
+
   }
 }
 
