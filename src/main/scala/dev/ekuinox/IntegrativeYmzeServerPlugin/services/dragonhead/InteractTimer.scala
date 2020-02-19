c122d5ecc971593ad7423827d67dbc91eb1cbfe5
ekuinox <depkey@me.com>
2020-02-19 14:36:06 +0900

    #25 InteractTimer（右クリ二度発射用）

---
+++ src/main/scala/dev/ekuinox/IntegrativeYmzeServerPlugin/services/dragonhead/InteractTimer.scala
@@ -0,0 +1,24 @@
+package dev.ekuinox.IntegrativeYmzeServerPlugin.services.dragonhead
+
+import org.bukkit.entity.Player
+import org.bukkit.persistence.PersistentDataType
+import org.bukkit.scheduler.BukkitRunnable
+
+object InteractTimer {
+  private val KEY = "interact-timer"
+  private val TYPE = PersistentDataType.STRING
+  private val DELAY = 1
+  implicit class PlayerWithInteractTimer(player: Player)(implicit service: DragonHeadService) {
+    private val namespacedKey = service.makeNamespacedKey(KEY)
+    private val container = player.getPersistentDataContainer
+    // タイマが停止しているか
+    def isInteractTimerStop: Boolean = container.has(namespacedKey, TYPE)
+    // タイマを開始させる
+    def startInteractTimer(): Unit = {
+      new BukkitRunnable {
+        override def run(): Unit = container.remove(namespacedKey)
+      }.runTaskLaterAsynchronously(service.getPlugin, DELAY)
+    }
+    def withInteractTimerStop: Option[Player] = if (isInteractTimerStop) Some(player) else None
+  }
+}
