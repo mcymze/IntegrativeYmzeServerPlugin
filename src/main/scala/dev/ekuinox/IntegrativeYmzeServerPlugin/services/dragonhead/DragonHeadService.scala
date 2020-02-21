198404918568131b3dd1498da061194760dae452
ekuinox <depkey@me.com>
2020-02-21 16:56:37 +0900

    リスナの登録

--- src/main/scala/dev/ekuinox/IntegrativeYmzeServerPlugin/services/dragonhead/DragonHeadService.scala
+++ src/main/scala/dev/ekuinox/IntegrativeYmzeServerPlugin/services/dragonhead/DragonHeadService.scala
@@ -1,6 +1,7 @@
 package dev.ekuinox.IntegrativeYmzeServerPlugin.services.dragonhead
 
 import dev.ekuinox.IntegrativeYmzeServerPlugin.Main
+import dev.ekuinox.IntegrativeYmzeServerPlugin.services.dragonhead.listeners.PlayerInteractEventListener
 import dev.ekuinox.IntegrativeYmzeServerPlugin.utils.{EventListener, Service}
 
 class DragonHeadService(implicit main: Main) extends Service {
@@ -8,5 +9,7 @@ class DragonHeadService(implicit main: Main) extends Service {
   /**
    * Listener 管理
    */
-  override val eventListeners: Seq[EventListener] = Seq()
+  override val eventListeners: Seq[EventListener] = Seq(
+    new PlayerInteractEventListener()(this)
+  )
 }
