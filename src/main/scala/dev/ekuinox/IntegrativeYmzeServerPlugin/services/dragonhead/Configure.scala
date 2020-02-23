81a8a0ee5196fbfa8038003f444e5372646e0b6f
ekuinox <depkey@me.com>
2020-02-23 10:04:13 +0900

    configureの値をもっと短く拾って来られるようにした

--- src/main/scala/dev/ekuinox/IntegrativeYmzeServerPlugin/services/dragonhead/Configure.scala
+++ src/main/scala/dev/ekuinox/IntegrativeYmzeServerPlugin/services/dragonhead/Configure.scala
@@ -1,18 +1,26 @@
 package dev.ekuinox.IntegrativeYmzeServerPlugin.services.dragonhead
 
+import org.bukkit.configuration.file.FileConfiguration
+
 object Configure {
+
+  /**
+   * 相対パスを受け取る => FileConfiguration, 絶対パス => 任意の型
+   */
+  private def get[T](path: String)(callback: (FileConfiguration, String) => T)(implicit service: DragonHeadService): T =
+    callback(service.getPlugin.getConfig, service.makeConfigurePath(path))
 
   /**
    * 着弾時に破壊する範囲
    */
   def getExplosiveRadius()(implicit service: DragonHeadService): Float =
-    service.getPlugin.getConfig.getDouble(service.makeConfigurePath("explosive-radius"), 0.0d).toFloat
+    get("explosive-radius")((conf, path) => conf.getDouble(path, 0.0d)).toFloat
 
   /**
    * 着弾時に着火するか
    */
-  def isTriggerFire()(implicit service: DragonHeadService): Boolean = {
-    service.getPlugin.getConfig.getBoolean(service.makeConfigurePath("is-trigger-fire"), false)
+  def isTriggerFire()(implicit service: DragonHeadService): Boolean =
+    get("is-trigger-fire")((conf, path) => conf.getBoolean(path, false))
   }
 
 }
