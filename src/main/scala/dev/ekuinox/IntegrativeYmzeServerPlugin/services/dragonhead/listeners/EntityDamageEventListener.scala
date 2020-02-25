b478502d6075650461381ab389e1627c7a732099
ekuinox <depkey@me.com>
2020-02-26 01:39:00 +0900

    #47 ジェッパによる飛行中のダメージを無効にするように

--- src/main/scala/dev/ekuinox/IntegrativeYmzeServerPlugin/services/dragonhead/listeners/EntityDamageEventListener.scala
+++ src/main/scala/dev/ekuinox/IntegrativeYmzeServerPlugin/services/dragonhead/listeners/EntityDamageEventListener.scala
@@ -1,6 +1,6 @@
 package dev.ekuinox.IntegrativeYmzeServerPlugin.services.dragonhead.listeners
 
-import dev.ekuinox.IntegrativeYmzeServerPlugin.services.dragonhead.DragonHeadService
+import dev.ekuinox.IntegrativeYmzeServerPlugin.services.dragonhead.{Configure, DragonHeadService}
 import dev.ekuinox.IntegrativeYmzeServerPlugin.utils.EventListener
 import org.bukkit.entity.{EntityType, Player}
 import org.bukkit.event.EventHandler
@@ -12,6 +12,7 @@ class EntityDamageEventListener(implicit service: DragonHeadService) extends Eve
 
   @EventHandler
   def onEntityDamage(event: EntityDamageEvent): Unit = {
+    import dev.ekuinox.IntegrativeYmzeServerPlugin.services.dragonhead.ImplicitPlayerExtended._
 
     // 落下ダメージ以外は無視
     if (event.getCause != EntityDamageEvent.DamageCause.FALL) return
@@ -20,7 +21,13 @@ class EntityDamageEventListener(implicit service: DragonHeadService) extends Eve
     if (event.getEntityType != EntityType.PLAYER) return
 
     Try(event.getEntity.asInstanceOf[Player]).toOption.foreach { player =>
-      event.setCancelled(true)
+      // ジェッパによる飛行中ではなかった
+      if (!player.isActiveDragonFlight) return
+
+      // 無効に
+      player.deactivateDragonFlight()
+
+      event.setCancelled(Configure.isIgnoredJetPackFallingDamage)
     }
 
   }
