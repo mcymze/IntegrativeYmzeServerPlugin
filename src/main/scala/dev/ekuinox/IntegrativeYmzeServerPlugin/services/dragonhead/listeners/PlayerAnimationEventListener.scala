013de7ebe11f3a2726bbc11de73e4c43ce37b96d
ekuinox <depkey@me.com>
2020-02-25 17:51:47 +0900

    #37 ドラゴン頭を所持して左クリしたときに飛ぶようにした
    と思ったけど、右クリで手を降ることもあるっぽくて競合してる

--- src/main/scala/dev/ekuinox/IntegrativeYmzeServerPlugin/services/dragonhead/listeners/PlayerAnimationEventListener.scala
+++ src/main/scala/dev/ekuinox/IntegrativeYmzeServerPlugin/services/dragonhead/listeners/PlayerAnimationEventListener.scala
@@ -1,7 +1,10 @@
 package dev.ekuinox.IntegrativeYmzeServerPlugin.services.dragonhead.listeners
 
 import dev.ekuinox.IntegrativeYmzeServerPlugin.services.dragonhead.DragonHeadService
+import dev.ekuinox.IntegrativeYmzeServerPlugin.services.dragonhead.Configure._
+import dev.ekuinox.IntegrativeYmzeServerPlugin.services.dragonhead.permissions.JetPack
 import dev.ekuinox.IntegrativeYmzeServerPlugin.utils.EventListener
+import org.bukkit.Material
 import org.bukkit.event.EventHandler
 import org.bukkit.event.player.{PlayerAnimationEvent, PlayerAnimationType}
 
@@ -12,7 +15,16 @@ class PlayerAnimationEventListener(implicit service: DragonHeadService) extends 
     // 左クリック
     if (event.getAnimationType != PlayerAnimationType.ARM_SWING) return
 
+    if (!isEnabledJetPack) return
 
+    val player = event.getPlayer
+
+    if (!player.hasPermission(JetPack)) return
+
+    // 左手にドラゴン頭を持っていないと発動しない
+    if (player.getInventory.getItemInOffHand.getType != Material.DRAGON_HEAD) return
+
+    player.setVelocity(player.getEyeLocation.getDirection.multiply(getJetPackMultiplyVelocity))
 
   }
 
