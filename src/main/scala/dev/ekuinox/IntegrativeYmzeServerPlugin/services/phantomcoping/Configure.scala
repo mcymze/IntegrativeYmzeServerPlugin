1f2b62222e8cc06933a2ef51ce56fee7132720cb
ekuinox <depkey@me.com>
2020-02-25 17:00:36 +0900

    implicit classをやめ、Service#getConfを使うように変更

--- src/main/scala/dev/ekuinox/IntegrativeYmzeServerPlugin/services/phantomcoping/Configure.scala
+++ src/main/scala/dev/ekuinox/IntegrativeYmzeServerPlugin/services/phantomcoping/Configure.scala
@@ -20,44 +20,43 @@ object Configure {
     } yield TargetItem(material, ticks.toLong)
   }
 
-  implicit class ServiceWithConfigure(service: PhantomCopeService) {
-    private val configure = service.getPlugin.getConfig
+  /**
+   * 対象のTargetItemのリスト
+   */
+  def getTargetItems(implicit service: PhantomCopeService): List[TargetItem] =
+    service.getConf("items")((conf, path) => conf.getMapList(path).asScala.map(_.asScala).flatMap(map => for {
+      map <- Try(map.asInstanceOf[mutable.Map[String, Any]]).toOption
+      item <- TargetItem.fromMap(map)
+    } yield item).toList)
 
-    private def makeKey(key: String): String = service.makeConfigurePath(key)
+  /**
+   * 機能有効時に表示されるメッセージ
+   */
+  def getActivationMessage(implicit service: PhantomCopeService): Option[String] =
+    service.getConf("messages.activation")((conf, path) => Option(conf.getString(path)))
 
-    /**
-     * 対象のTargetItemのリスト
-     */
-    def getTargetItems: List[TargetItem] =
-      configure.getMapList(makeKey("items")).asScala.map(_.asScala).flatMap(map => for {
-        map <- Try(map.asInstanceOf[mutable.Map[String, Any]]).toOption
-        item <- TargetItem.fromMap(map)
-      } yield item).toList
+  /**
+   * 機能無効時に表示されるメッセージ
+   */
+  def getDeactivationMessage(implicit service: PhantomCopeService): Option[String] =
+    service.getConf("messages.deactivation")((conf, path) => Option(conf.getString(path)))
 
-    /**
-     * 機能有効時に表示されるメッセージ
-     */
-    def getActivationMessage: Option[String] = Option(configure.getString(makeKey("messages.activation")))
+  /**
+   * itemの効果を無視するか
+   */
+  def isIgnoredItemEffect(implicit service: PhantomCopeService): Boolean =
+    service.getConf("ignore-item-effect")((conf, path) => conf.getBoolean(path, true))
 
-    /**
-     * 機能無効時に表示されるメッセージ
-     */
-    def getDeactivationMessage: Option[String] = Option(configure.getString(makeKey("messages.deactivation")))
+  /**
+   * ファントムを屋内で沸かせない（ガラスの下だと湧いてしまうので）
+   */
+  def isCancelingSpawningPhantomIndoor(implicit service: PhantomCopeService): Boolean =
+    service.getConf("cancel-indoor")((conf, path) => conf.getBoolean(path, true))
 
-    /**
-     * itemの効果を無視するか
-     */
-    def isIgnoredItemEffect: Boolean = configure.getBoolean(makeKey("ignore-item-effect"), true)
+  /**
+   * 屋内かの検索をかける最大高度
+   */
+  def getMaxAltitude(implicit service: PhantomCopeService): Int =
+    service.getConf("max-altitude")((conf, path) => conf.getInt(path, 256))
 
-    /**
-     * ファントムを屋内で沸かせない（ガラスの下だと湧いてしまうので）
-     */
-    def isCancelingSpawningPhantomIndoor: Boolean = configure.getBoolean(makeKey("cancel-indoor"), true)
-
-    /**
-     * 屋内かの検索をかける最大高度
-     */
-    def getMaxAltitude: Int = configure.getInt(makeKey("max-altitude"), 256)
-
-  }
 }
