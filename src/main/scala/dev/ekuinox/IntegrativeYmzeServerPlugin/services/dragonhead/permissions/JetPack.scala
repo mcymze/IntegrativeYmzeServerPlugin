b37160bcb5e124bc06c5a3bdcefc0c9aae3d9280
ekuinox <depkey@me.com>
2020-02-25 17:36:07 +0900

    #37 Permissionの用意

---
+++ src/main/scala/dev/ekuinox/IntegrativeYmzeServerPlugin/services/dragonhead/permissions/JetPack.scala
@@ -0,0 +1,8 @@
+package dev.ekuinox.IntegrativeYmzeServerPlugin.services.dragonhead.permissions
+
+import dev.ekuinox.IntegrativeYmzeServerPlugin.utils.Permissions.Permission
+
+/**
+ * エンドラ頭を利用してJetPack効果を得る権限
+ */
+object JetPack extends Permission("jetpack")
